3ad4ed3bcc85d79a4849aa541732a201dac98515
Dominic Plouffe <dominic@dplouffe.ca>
2021-08-06 19:09:32 -0400

    changed log name

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	}
 	rand.Seed(time.Now().UnixNano())
 
-	f, err := os.OpenFile("zimua_mob.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("zimua_mob._v2.0log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
